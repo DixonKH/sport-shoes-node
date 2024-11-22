2781b6b1e98a86e2ba4b41101eb6b7411f8e9c30
DixonKH <khaydarovdilshod9@gmail.com>
2024-11-22 19:46:57 +0900

    feat: connection to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 console.log("Executed");
 import dotenv from "dotenv";  
 dotenv.config();
-console.log("PORT: ", process.env.PORT);
-console.log("MONGO_URL: ", process.env.MONGO_URL);
+
+import mongoose from "mongoose";
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3001;
+})
+.catch((err) => console.log("ERROR on connection MongoDB: ", err));
+
+
+
   
